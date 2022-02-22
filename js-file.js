219a5f67be5abcf9ae20a21448879ce4d8c13806
Derek Nunes <dereknunes28@gmail.com>
2022-02-21 23:51:54 -0600

    added some notes and button functionality. bugs are present

--- js-file.js
+++ js-file.js
@@ -1,5 +1,21 @@
+/* Using toCalc to store inputed numbers
+    - Add first number on display to toCalc when an operator is pressed
+        -When using percent, function will multiply by 100 and input value on display. when normal operator is selected, that number will be added to toCalc, so will not be stored until operator is selected.
+        - Same with plus/minus except multiply by -1
+    - Order: 
+        1: display number
+        2: select operator. This adds the display number to toCalc[0], and the operator symbol to toCalc[1] 
+        3: another display number 
+        4: select second operator. This adds the display number to ToCalc[2], runs the result between toCalc[0] and toCalc[2] using the operator assigned to toCalc[1]. Then clears toCalc (toCalc = [];), adds the result to toCalc[0], and adds the operator selected in step 4 to toCalc[1].
+        5: If equals is selected instead of a second operator, this adds the display value to toCalc[2], runs and displays the result between toCalc[0] and toCalc[2] using the operator assigned to toCalc[1]. then clears toCalc (toCalc = [];), and adds the result to toCalc[0]
+        6: an operator selected after 'equals' assigns the selected operator to toCalc[1] and the display number to toCalc[0]
+
+        Will need to add several if/else if statements to define different scenarios when toCalc[0],[1],or [2] are defined if (toCalc[i] !== defined) ... 
+
+        when doing an operater, when you press = multiple times it repeats the operator.
+*/
 let displayValue = '';
-let toCalc = ['8'];
+let toCalc = [];
 // const gridButtons = document.querySelector('.buttons-grid');
 
 // input numbers into display
@@ -8,6 +24,30 @@ selectNumbers();
 // Disables decimal once selected once
 document.querySelector('.b10').addEventListener('click', function(e) {
     document.querySelector('.b10').disabled = true;
+});
+
+// Backspace button
+document.querySelector('.backspace').addEventListener('click', function(e) {
+    let currentNumberLength = displayValue.length;
+    displayValue = displayValue.slice(0, currentNumberLength -1);
+    if (displayValue === '') { displayValue = '0' };
+    document.querySelector('.inputDisplay').textContent = displayValue;
+});
+
+// Plus/Minus button
+// bug type 9 numbers then minus works but minus at 0 returns Nan or can only fit 8 numbers and the negative
+document.querySelector('.plusMinus').addEventListener('click', function(e) {
+    let newDisplayValue = parseFloat(displayValue) * -1;
+    displayValue = `${newDisplayValue}`;
+    document.querySelector('.inputDisplay').textContent = displayValue;
+});
+
+// Percent button
+// bugs: some numbers too long for line
+document.querySelector('.percent').addEventListener('click', function(e) {
+    let newDisplayValue = parseFloat(displayValue) / 100;
+    displayValue= `${newDisplayValue}`;
+    document.querySelector('.inputDisplay').textContent = displayValue;
 });
 
 // To clear display
